5511942be2daf01a8cfc753011470f93c02c4732
Isabela <isabelaolsemann@gmail.com>
2025-11-18 17:53:21 -0300

    feat: Adiciona funções de multiplicação e saudação (exercicio_ts)

---
+++ 23_TypeScript/multiplicacao_saudacao.js
@@ -0,0 +1,11 @@
+// A função de multiplicação 
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+// A função de saudação 
+function saudacao(nome) {
+    return 'Olá ' + nome;
+}
+// Retorno
+console.log(multiplicacao(10, 3)); // Saída: 30
+console.log(saudacao('Isabela')); // Saída: Olá Maria
---
+++ 23_TypeScript/multiplicacao_saudacao.ts
@@ -0,0 +1,13 @@
+// A função de multiplicação 
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+// A função de saudação 
+function saudacao(nome: string): string {
+    return 'Olá, ' + nome;
+}
+
+// Retorno
+console.log(multiplicacao(10, 3)); // Saída: 30
+console.log(saudacao('Isabela'));    // Saída: Olá Maria
